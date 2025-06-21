e99ed871a9f4e65a71c124c354abb7b4771b82aa
seungchanlee123 <seungchanlee123@gmail.com>
2025-06-22 00:59:52 +0900

    🔥데이터베이스 연결 성공

---
+++ mysql-termproject/src/main/kotlin/kr/ac/kumoh/s20220832/mysql_termproject/MysqlTermprojectApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220832.mysql_termproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlTermprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlTermprojectApplication>(*args)
+}
---
+++ mysql-termproject/src/test/kotlin/kr/ac/kumoh/s20220832/mysql_termproject/MysqlTermprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220832.mysql_termproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlTermprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
